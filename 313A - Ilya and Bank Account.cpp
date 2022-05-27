81df1da74f32038e983b4b567f9d2c033161da6e
MD Shahadat Hossain Bhuian <103915287+Coder-Shahadat@users.noreply.github.com>
2022-05-28 00:00:22 +0600

    Update 313A - Ilya and Bank Account.cpp

--- 313A - Ilya and Bank Account.cpp
+++ 313A - Ilya and Bank Account.cpp
@@ -1,37 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
-using ll = long long;
 
 int main(){
-    ll t, n, temp;
-    cin >> t;
-    while (t--)
-    {
-        cin >> n;
-        if(n&1)
-            cout << "YES" << endl;
-        else{
-            bool odd = false;
-            temp = n;
-            while (temp > 2)
-            {
-                temp /= 2;
-                if (temp & 1)
-                {
-                    if (!(n % temp))
-                    {
-                        odd = true;
-                        break;
-                    }
-                }
-            }
-            if(odd)
-                cout << "YES" << endl;
-            else
-                cout << "NO"<<endl;
-        }
-        
+    int n;
+    cin >> n;
+    if(n>0){
+        cout << n << endl;
+        return 0;
     }
-    
+    else{
+        int Max = n / 10;
+        Max = max(Max, (n / 100 * 10 + n % 10));
+        cout << Max << endl;
+    }
     return 0;
 }
