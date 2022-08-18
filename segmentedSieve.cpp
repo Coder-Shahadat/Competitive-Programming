d53561c98250d9db8c3293eabbe50c8312bd9d48
MD Shahadat Hossain Bhuian <103915287+Coder-Shahadat@users.noreply.github.com>
2022-08-18 11:58:15 +0600

    Update segmentedSieve.cpp

--- segmentedSieve.cpp
+++ segmentedSieve.cpp
@@ -1,6 +1,17 @@
-#include <bits/stdc++.h>
+// Contest Platform: spoj
+// Contest Name : Problems/Prime Generator 
+// Problem Name : Prime Generator
+// Author: MD.Shahadat Hossain Bhuian
+// Codeforces userName : __Shahadat__
+// CodeChef userName : shahadat_43
+//---------------------------------------------
+
+#include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+#define endl "\n"
+#define pb push_back
+#define Faster ios_base::sync_with_stdio(false), cin.tie(NULL),cout.tie(NULL);
 const ll c = 1e6;
 
 vector<int> primes;
